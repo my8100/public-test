63e372ab66f5392381016d4c6ece7405b5738b64
my8100 <my8100@gmail.com>
2019-08-01 23:17:43 +0800

    Update setup_database.py

--- scrapydweb/utils/setup_database.py
+++ scrapydweb/utils/setup_database.py
@@ -139,9 +139,10 @@ def setup_postgresql(username, password, host, port):
     except (ImportError, AssertionError):
         sys.exit("Run command: %s" % install_command)
     print(username, password, host, port)
-    conn = psycopg2.connect(host=host, port=int(port), user=username, password=password)
+    # conn = psycopg2.connect(host=host, port=int(port), user=username, password=password)
     # conn = psycopg2.connect(host=host, port=int(port), user=username, password=None)
     # conn = psycopg2.connect(host=host, port=int(port), user=username, password=password, dbname='scrapydweb_apscheduler')
+    conn = psycopg2.connect(host=host, port=int(port), user=username, password=password, dbname=None)
     conn.set_isolation_level(0)  # https://wiki.postgresql.org/wiki/Psycopg2_Tutorial
     cur = conn.cursor()
     print(cur)
